4d1e5b2b92ea908880c2ac4b5db0dfb60a4a4137
opilane <opilane@HP-CZC2349HTY>
2023-01-25 15:20:47 +0200

    Add project files.

---
+++ PythonApplication5.py
@@ -0,0 +1,20 @@
+﻿#0
+
+text=(input("Esimene nimi: "))
+i = 0
+while i < len(text):
+    print(text[i])
+    i += 1
+print("---------------------------------")
+for i in text:
+    print(i)
+
+#21
+while True:
+    s=str(input(": "))
+    L = s.split()
+    
+    lon = max(L, key =len)
+    short = min(L, key=len)
+    print(f"Longest: {lon}, shortest: {short}")
+
